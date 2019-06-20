c25e903642d5ccd9beedfce40f00dcc0a13e832a
Patrick Hulce <patrick.hulce@gmail.com>
2019-06-20 17:24:25 -0500

    tests: typecheck and lint jsx

--- src/ui/app.jsx
+++ src/ui/app.jsx
@@ -13,15 +13,21 @@ const messagesForLoadingState = {
 export const App = () => {
   const [loadingState, setLoadingState] = useState(/** @type {LoadingState} */ ('loading'));
 
-  useEffect(async () => {
-    try {
-      const response = await fetch('/v1/projects');
-      if (response.status !== 200) throw new Error('Could not connect.');
-      setLoadingState('connected');
-    } catch (err) {
-      setLoadingState('error');
-    }
-  }, []);
+  useEffect(
+    () => {
+      // Wrap in IIFE because the return value of useEffect should be a cleanup function, not a Promise.
+      (async () => {
+        try {
+          const response = await fetch('/v1/projects');
+          if (response.status !== 200) throw new Error('Could not connect.');
+          setLoadingState('connected');
+        } catch (err) {
+          setLoadingState('error');
+        }
+      })();
+    },
+    /* no state dependencies, only run this connection check on initial mount */ []
+  );
 
   return <h1>Lighthouse CI {messagesForLoadingState[loadingState]}</h1>;
 };

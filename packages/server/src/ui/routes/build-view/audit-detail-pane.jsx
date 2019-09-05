e70bc4ff02b16f26fd4c9eeffc38ece571420224
Patrick Hulce <patrick.hulce@gmail.com>
2019-09-05 16:29:25 -0500

    feat: scroll to selected audit

--- packages/server/src/ui/routes/build-view/audit-detail-pane.jsx
+++ packages/server/src/ui/routes/build-view/audit-detail-pane.jsx
@@ -7,11 +7,14 @@
 import {h} from 'preact';
 import './audit-detail-pane.css';
 import clsx from 'clsx';
+import {usePreviousValue} from '../../hooks/use-previous-value';
+import {useEffect, useRef} from 'preact/hooks';
 
 /** @param {{audit: LH.AuditResult, selectedAuditId: string|null, key?: string}} props */
 const Audit = props => {
   return (
     <div
+      id={`audit-detail-pane-audit--${props.audit.id}`}
       className={clsx('audit-detail-pane__audit', {
         'audit-detail-pane__audit--selected': props.selectedAuditId === props.audit.id,
       })}
@@ -27,13 +30,29 @@ const Audit = props => {
  * @param {{selectedAuditId: string, setSelectedAuditId: (id: string|null) => void, lhr: LH.Result, baseLhr?: LH.Result}} props
  */
 export const AuditDetailPane = props => {
+  /** @type {import('preact').Ref<HTMLElement|undefined>} */
+  const paneElementRef = useRef(undefined);
+  const previouslySelectedAuditId = usePreviousValue(props.selectedAuditId);
+
+  // Scroll to the selected audit *when it changes*
+  useEffect(() => {
+    const auditId = props.selectedAuditId;
+    const paneElement = paneElementRef.current;
+    if (!paneElement || !auditId || auditId === previouslySelectedAuditId) return;
+
+    const childElement = paneElement.querySelector(`#audit-detail-pane-audit--${auditId}`);
+    if (!childElement || !(childElement instanceof HTMLElement)) return;
+
+    paneElement.scrollTo(0, childElement.offsetTop);
+  }, [props.selectedAuditId, previouslySelectedAuditId]);
+
   return (
-    <div className="audit-detail-pane">
+    <div className="audit-detail-pane" ref={el => (paneElementRef.current = el)}>
       <div className="audit-detail-pane__close" onClick={() => props.setSelectedAuditId(null)}>
         x
       </div>
-      {Object.values(props.lhr.audits).map(audit => {
-        return <Audit key={audit.id} audit={audit} selectedAuditId={props.selectedAuditId} />;
+      {Object.entries(props.lhr.audits).map(([id, audit]) => {
+        return <Audit key={id} audit={{...audit, id}} selectedAuditId={props.selectedAuditId} />;
       })}
     </div>
   );

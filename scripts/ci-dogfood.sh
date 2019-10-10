f305a4f7f05f2179e65278c3e6c81e7e0a8e83f7
Patrick Hulce <patrick.hulce@gmail.com>
2019-10-10 18:00:55 -0500

    tests(ci): add assert to travis flow

--- scripts/ci-dogfood.sh
+++ scripts/ci-dogfood.sh
@@ -7,23 +7,31 @@ if [[ "$TRAVIS_NODE_VERSION" != "10" ]]; then
   exit 0;
 fi
 
-set -euox pipefail
+set -ox pipefail
 
 # Start up our LHCI server.
 yarn start:server --port=9009 &
 # Wait for the server to start before hitting it with data.
-sleep 10
+sleep 5
 
 # Seed the database with some data for us to audit.
 yarn start:seed-database
 # Collect our LHCI results.
 rm -rf .lighthouseci/
 for url in $(LHCI_ROOT_URL=http://localhost:9009 node ./scripts/ci-dogfood-get-urls.js); do
-  yarn start collect "--url=$url" --additive
+  yarn start collect "--url=$url" --additive || exit 1
 done
 
-# Upload the results to our canary server.
-yarn start upload --serverBaseUrl="$LHCI_CANARY_SERVER_URL" --token="$LHCI_CANARY_SERVER_TOKEN"
+# Assert our results, but don't fail the build yet.
+yarn start assert --rc-file=lighthouserc.json
+EXIT_CODE=$?
+
+if [[ -n "$LHCI_CANARY_SERVER_URL" ]]; then
+  # Upload the results to our canary server.
+  yarn start upload --serverBaseUrl="$LHCI_CANARY_SERVER_URL" --token="$LHCI_CANARY_SERVER_TOKEN"
+fi
 
 # Kill the LHCI server from earlier.
 kill $!
+
+exit $EXIT_CODE

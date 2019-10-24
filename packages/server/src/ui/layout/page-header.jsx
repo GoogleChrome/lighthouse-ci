cf77f868ab07a3943ee1adf647baa9c95a18ab2d
Patrick Hulce <patrick.hulce@gmail.com>
2019-10-24 11:46:13 -0500

    refactor(server): simplify sidebar component

--- packages/server/src/ui/layout/page-header.jsx
+++ packages/server/src/ui/layout/page-header.jsx
@@ -4,7 +4,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-import {h, Fragment, VNode} from 'preact';
+import {h, VNode} from 'preact';
 import clsx from 'clsx';
 import './page-header.css';
 import {useProject} from '../hooks/use-api-data';

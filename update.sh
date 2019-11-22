3cde539098321e86c6d27af6629b56ee5345afb9
Patrick Hulce <patrick.hulce@gmail.com>
2019-11-22 13:51:44 -0600

    chore: update lhci server

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NEXT_VERSION=$(yarn info @lhci/server@next | grep 'next:' -A 1 | tail -n 1 | grep -o "'.*'" | sed s/\'//g)
+NEXT_VERSION=$(yarn info @lhci/server | grep 'latest:' -A 1 | tail -n 1 | grep -o "'.*'" | sed s/\'//g)
 node write-version.js "$NEXT_VERSION"
 
 git add -A

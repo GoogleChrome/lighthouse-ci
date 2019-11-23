5d82544f71111957912b03f55ab340dcbbc67aa9
Patrick Hulce <patrick.hulce@gmail.com>
2019-11-23 09:44:54 -0600

    docs(recipes): add docker update script

---
+++ docs/recipes/docker-server/update-dockerhub.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CURRENT_VERSION=$(node -e "console.log(require('./package.json').dependencies['@lhci/server'])")
+# NEXT_VERSION=$(yarn info @lhci/server | grep 'latest:' -A 1 | tail -n 1 | grep -o "'.*'" | sed s/\'//g)
+NEXT_VERSION=0.3.3
+
+if [[ "$CURRENT_VERSION" == "$NEXT_VERSION" ]]; then
+  echo "Version is already at $NEXT_VERSION, exiting..."
+  exit 0
+fi
+
+printf "Previous version is $CURRENT_VERSION\n"
+printf "Publish docker version $NEXT_VERSION?\n"
+read -n 1 -p "Press any key to continue, Ctrl+C to exit..."
+
+printf "\nUpdating local files...\n\n"
+
+sed "s/$CURRENT_VERSION/$NEXT_VERSION/" package.json kubernetes/lhci-pod.yml
+git diff .
+
+printf "Continue publishing $NEXT_VERSION?\n"
+read -n 1 -p "Press any key to continue, Ctrl+C to exit..."
+
+printf "\nPublishing!"
+sleep 2 # Give the user one more chance to Ctrl+C
+
+docker image build -t lhci .
+docker tag lhci:latest "patrickhulce/lhci-server:$NEXT_VERSION"
+docker tag "patrickhulce/lhci-server:$NEXT_VERSION" patrickhulce/lhci-server:latest
+docker push "patrickhulce/lhci-server:$NEXT_VERSION"
+docker push patrickhulce/lhci-server:latest

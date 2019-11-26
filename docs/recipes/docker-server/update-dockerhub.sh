9684772f44ab3c6fd9af1f4e50a3fc47934dafe3
Patrick Hulce <patrick.hulce@gmail.com>
2019-11-26 12:25:12 -0600

    docs(cli): add guide for --puppeteer-script

--- docs/recipes/docker-server/update-dockerhub.sh
+++ docs/recipes/docker-server/update-dockerhub.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 CURRENT_VERSION=$(node -e "console.log(require('./package.json').dependencies['@lhci/server'])")
-# NEXT_VERSION=$(yarn info @lhci/server | grep 'latest:' -A 1 | tail -n 1 | grep -o "'.*'" | sed s/\'//g)
-NEXT_VERSION=0.3.3
+NEXT_VERSION=$(yarn info @lhci/server | grep 'latest:' -A 1 | tail -n 1 | grep -o "'.*'" | sed s/\'//g)
 
 if [[ "$CURRENT_VERSION" == "$NEXT_VERSION" ]]; then
   echo "Version is already at $NEXT_VERSION, exiting..."

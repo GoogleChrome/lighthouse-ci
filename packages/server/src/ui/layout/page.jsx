d0e8a31675d272bf733b906fe99e23231b01f358
Patrick Hulce <patrick.hulce@gmail.com>
2020-01-15 15:53:13 -0600

    tests(server): expose PreactNode type on LHCI namespace

--- packages/server/src/ui/layout/page.jsx
+++ packages/server/src/ui/layout/page.jsx
@@ -4,14 +4,14 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-import {h, VNode, Fragment} from 'preact';
+import {h, Fragment} from 'preact';
 import {PageHeader} from './page-header.jsx';
 import {PageSidebar} from './page-sidebar.jsx';
 import {useState, useEffect} from 'preact/hooks';
 import {PageBody} from './page-body.jsx';
 
 /**
- * @param {{header?: Array<VNode> | VNode, headerLeft?: Array<VNode> | VNode, headerRight?: Array<VNode> | VNode, children: VNode | Array<VNode>}} props
+ * @param {{header?: LHCI.PreactNode, headerLeft?: LHCI.PreactNode, headerRight?: LHCI.PreactNode, children: LHCI.PreactNode}} props
  */
 export const Page = props => {
   const [isOpen, setIsOpen] = useState(false);

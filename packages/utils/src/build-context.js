95723701b35d1df1e70fd8bcc63a53ccee9cd398
Jim van der Voort <jimvandervoort@me.com>
2020-01-21 12:29:01 +0100

    Revert "feat(utils): incldue merge commits in build hash"
    This reverts commit 8893ea9624e59731ecf3d63c9afac34f7d6fe5fa.

--- packages/utils/src/build-context.js
+++ packages/utils/src/build-context.js
@@ -67,7 +67,7 @@ function getCurrentHash() {
   ]);
   if (envHash) return envHash;
 
-  const result = childProcess.spawnSync('git', ['rev-parse', 'HEAD'], {
+  const result = childProcess.spawnSync('git', ['rev-list', '--no-merges', '-n1', 'HEAD'], {
     encoding: 'utf8',
   });
   if (result.status !== 0) {

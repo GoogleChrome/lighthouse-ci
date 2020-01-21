8baaabb19afd8e39a75b3fdbf7ddb57d588e862d
Patrick Hulce <patrick.hulce@gmail.com>
2020-01-21 13:33:08 -0600

    feat(server): add base d3 score graph

--- packages/server/src/ui/routes/project-dashboard-redesign/category-graphs.jsx
+++ packages/server/src/ui/routes/project-dashboard-redesign/category-graphs.jsx
@@ -4,40 +4,38 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-import {h} from 'preact';
+import {h, Fragment} from 'preact';
 import _ from '@lhci/utils/src/lodash.js';
 import {AsyncLoader} from '../../components/async-loader';
 import {Paper} from '../../components/paper.jsx';
 
 import './category-graphs.css';
-import {LoadingSpinner} from '../../components/loading-spinner';
+import {CategoryScoreGraph} from './category-score-graph';
 
 /** @typedef {import('./project-graphs-redesign.jsx').StatisticWithBuild} StatisticWithBuild */
 
 /** @param {{title: string, category: 'performance'|'pwa', statistics?: Array<StatisticWithBuild>, loadingState: import('../../components/async-loader').LoadingState, builds: LHCI.ServerCommand.Build[]}} props */
 export const CategoryGraphs = props => {
   return (
-    <AsyncLoader
-      loadingState={props.loadingState}
-      asyncData={props.statistics}
-      renderLoading={() => (
-        <Paper className="category-graphs">
-          <LoadingSpinner />
-        </Paper>
-      )}
-      render={allStats => {
-        return (
-          <Paper className="category-graphs">
-            <div className="category-graphs__header">
-              <h3 className="category-graphs__title">{props.title}</h3>
-            </div>
-            <pre>
-              Data goes here...
-              {JSON.stringify(allStats, null, 2)}
-            </pre>
-          </Paper>
-        );
-      }}
-    />
+    <Paper className="category-graphs">
+      <div className="category-graphs__header">
+        <h3 className="category-graphs__title">{props.title}</h3>
+      </div>
+      <div className="category-graphs__body">
+        <AsyncLoader
+          loadingState={props.loadingState}
+          asyncData={props.statistics}
+          render={allStats => (
+            <Fragment>
+              <CategoryScoreGraph {...props} statistics={allStats} />
+              <pre>
+                Data goes here...
+                {JSON.stringify(allStats, null, 2)}
+              </pre>
+            </Fragment>
+          )}
+        />
+      </div>
+    </Paper>
   );
 };
---
+++ packages/server/src/ui/routes/project-dashboard-redesign/category-score-graph.jsx
@@ -0,0 +1,96 @@
+/**
+ * @license Copyright 2020 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import {h} from 'preact';
+import * as d3 from 'd3';
+
+import './category-score-graph.css';
+import {useRef, useEffect} from 'preact/hooks';
+
+/** @typedef {import('./project-graphs-redesign.jsx').StatisticWithBuild} StatisticWithBuild */
+
+/**
+ *
+ * @param {HTMLElement} rootEl
+ * @param {Array<StatisticWithBuild>} statistics
+ */
+function render(rootEl, statistics) {
+  const margin = 10;
+  const marginRight = 50;
+  const height = rootEl.clientHeight;
+  const width = rootEl.clientWidth;
+  const graphWidth = width - margin - marginRight;
+  const graphHeight = height - margin * 2;
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, statistics.length - 1])
+    .range([0, graphWidth]);
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, 100])
+    .range([graphHeight, 0]);
+
+  const yAxis = d3
+    .axisLeft(yScale)
+    .ticks(6)
+    .tickSize(0);
+
+  /** @type {() => import('d3').Line<StatisticWithBuild>} */
+  const d3Line = d3.line;
+  const line = d3Line()
+    .x((d, i) => xScale(i))
+    .y(d => yScale(d.value * 100))
+    .curve(d3.curveMonotoneX);
+
+  d3.select(rootEl)
+    .selectAll('*')
+    .remove();
+
+  const svg = d3
+    .select(rootEl)
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('style', `transform: translate(${margin}px, ${margin}px)`);
+
+  svg
+    .append('g')
+    .attr('class', 'y-axis')
+    .attr('style', `transform: translateX(${width - marginRight / 2}px)`)
+    .call(yAxis);
+
+  svg
+    .append('path')
+    .datum(statistics)
+    .attr('class', 'score-line')
+    .attr('d', line);
+}
+
+/** @param {{statistics: Array<StatisticWithBuild>}} props */
+const Graph = props => {
+  const graphElRef = useRef(/** @type {HTMLElement|undefined} */ (undefined));
+
+  useEffect(() => {
+    if (!graphElRef.current) return;
+    render(graphElRef.current, props.statistics);
+  }, [props.statistics.length]);
+
+  return <div className="category-score-graph__score-graph" ref={graphElRef} />;
+};
+
+/** @param {{category: 'performance'|'pwa', statistics: Array<StatisticWithBuild>}} props */
+export const CategoryScoreGraph = props => {
+  const statistics = props.statistics.filter(s => s.name === `category_${props.category}_average`);
+
+  return (
+    <div className="category-score-graph">
+      <h2>Overview</h2>
+      <Graph statistics={statistics} />
+    </div>
+  );
+};

59c8226c2c57b7059683fbc524e9b673ecfe4a92
Patrick Hulce <patrick.hulce@gmail.com>
2020-02-04 22:01:39 -0600

    tests(server): automatically spin up storybook server in jest

--- packages/server/test/storybook-setup.js
+++ packages/server/test/storybook-setup.js
@@ -1,19 +1,18 @@
 /**
- * @license Copyright 2019 Google Inc. All Rights Reserved.
+ * @license Copyright 2020 Google Inc. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 'use strict';
 
-module.exports = {
-  testRunner: require.resolve('jest-circus/runner'),
-  globalSetup: require.resolve('./packages/server/test/storybook-setup.js'),
-  globalTeardown: require.resolve('./packages/server/test/storybook-teardown.js'),
-  transform: {
-    '.*jsx$': 'babel-jest',
-  },
-  moduleNameMapper: {
-    '\\.css$': 'identity-obj-proxy',
-    '\\.(svg|png|jpg|jpeg)$': '<rootDir>/packages/server/test/__mocks__/file-mock.js',
-  },
+const path = require('path');
+const FallbackServer = require('../../cli/src/collect/fallback-server');
+
+const STORYBOOK_STATIC = path.join(__dirname, '../storybook-static');
+
+module.exports = async () => {
+  const server = new FallbackServer(STORYBOOK_STATIC, false);
+  await server.listen();
+  process.env.STORYBOOK_PORT = server.port;
+  process.fallbackServer = server;
 };
--- packages/server/test/storybook-teardown.js
+++ packages/server/test/storybook-teardown.js
@@ -1,19 +1,10 @@
 /**
- * @license Copyright 2019 Google Inc. All Rights Reserved.
+ * @license Copyright 2020 Google Inc. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 'use strict';
 
-module.exports = {
-  testRunner: require.resolve('jest-circus/runner'),
-  globalSetup: require.resolve('./packages/server/test/storybook-setup.js'),
-  globalTeardown: require.resolve('./packages/server/test/storybook-teardown.js'),
-  transform: {
-    '.*jsx$': 'babel-jest',
-  },
-  moduleNameMapper: {
-    '\\.css$': 'identity-obj-proxy',
-    '\\.(svg|png|jpg|jpeg)$': '<rootDir>/packages/server/test/__mocks__/file-mock.js',
-  },
+module.exports = async () => {
+  await process.fallbackServer.close();
 };

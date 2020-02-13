4dcae09b21646809acf25340b76e144661805990
Patrick Hulce <patrick.hulce@gmail.com>
2020-02-13 12:08:22 -0600

    feat(server): add statistics for audit group pass counts

--- packages/server/src/api/statistic-definitions.js
+++ packages/server/src/api/statistic-definitions.js
@@ -5,6 +5,8 @@
  */
 'use strict';
 
+const {computeRepresentativeRuns} = require('@lhci/utils/src/representative-runs');
+
 /** @typedef {(lhrs: Array<LH.Result>) => ({value: number})} StatisticFn */
 
 /** @param {Array<number>} values */
@@ -69,6 +71,38 @@ function categoryScoreMinOrMax(categoryId, type) {
   };
 }
 
+/**
+ * @param {string} groupId
+ * @param {'pass'|'fail'|'na'} type
+ * @return {StatisticFn}
+ */
+function auditGroupCountOfMedianLhr(groupId, type) {
+  return lhrs => {
+    const [medianLhr] = computeRepresentativeRuns([lhrs.map(lhr => [lhr, lhr])]);
+    if (!medianLhr) return {value: -1};
+
+    // Start out with -1 as "no data available"
+    let count = -1;
+    for (const category of Object.values(medianLhr.categories)) {
+      for (const auditRef of category.auditRefs || []) {
+        if (auditRef.group !== groupId) continue;
+        const audit = medianLhr.audits[auditRef.id];
+        if (!audit) continue;
+
+        // Once we find our first candidate audit, set the count to 0.
+        if (count === -1) count = 0;
+
+        const {score, scoreDisplayMode} = audit;
+        if (scoreDisplayMode === 'notApplicable' && type === 'na') count++;
+        if (scoreDisplayMode === 'binary' && score === 1 && type === 'pass') count++;
+        if (scoreDisplayMode === 'binary' && score !== 1 && type === 'fail') count++;
+      }
+    }
+
+    return {value: count};
+  };
+}
+
 /** @type {Record<LHCI.ServerCommand.StatisticName, StatisticFn>} */
 const definitions = {
   audit_interactive_average: auditNumericValueAverage('interactive'),
@@ -89,6 +123,48 @@ const definitions = {
   category_seo_max: categoryScoreMinOrMax('seo', 'max'),
   category_accessibility_max: categoryScoreMinOrMax('accessibility', 'max'),
   'category_best-practices_max': categoryScoreMinOrMax('best-practices', 'max'),
+  'auditgroup_pwa-fast-reliable_pass': auditGroupCountOfMedianLhr('pwa-fast-reliable', 'pass'),
+  'auditgroup_pwa-fast-reliable_fail': auditGroupCountOfMedianLhr('pwa-fast-reliable', 'fail'),
+  'auditgroup_pwa-fast-reliable_na': auditGroupCountOfMedianLhr('pwa-fast-reliable', 'na'),
+  'auditgroup_pwa-installable_pass': auditGroupCountOfMedianLhr('pwa-installable', 'pass'),
+  'auditgroup_pwa-installable_fail': auditGroupCountOfMedianLhr('pwa-installable', 'fail'),
+  'auditgroup_pwa-installable_na': auditGroupCountOfMedianLhr('pwa-installable', 'na'),
+  'auditgroup_pwa-optimized_pass': auditGroupCountOfMedianLhr('pwa-optimized', 'pass'),
+  'auditgroup_pwa-optimized_fail': auditGroupCountOfMedianLhr('pwa-optimized', 'fail'),
+  'auditgroup_pwa-optimized_na': auditGroupCountOfMedianLhr('pwa-optimized', 'na'),
+  'auditgroup_a11y-best-practices_pass': auditGroupCountOfMedianLhr('a11y-best-practices', 'pass'),
+  'auditgroup_a11y-best-practices_fail': auditGroupCountOfMedianLhr('a11y-best-practices', 'fail'),
+  'auditgroup_a11y-best-practices_na': auditGroupCountOfMedianLhr('a11y-best-practices', 'na'),
+  'auditgroup_a11y-color-contrast_pass': auditGroupCountOfMedianLhr('a11y-color-contrast', 'pass'),
+  'auditgroup_a11y-color-contrast_fail': auditGroupCountOfMedianLhr('a11y-color-contrast', 'fail'),
+  'auditgroup_a11y-color-contrast_na': auditGroupCountOfMedianLhr('a11y-color-contrast', 'na'),
+  'auditgroup_a11y-names-labels_pass': auditGroupCountOfMedianLhr('a11y-names-labels', 'pass'),
+  'auditgroup_a11y-names-labels_fail': auditGroupCountOfMedianLhr('a11y-names-labels', 'fail'),
+  'auditgroup_a11y-names-labels_na': auditGroupCountOfMedianLhr('a11y-names-labels', 'na'),
+  'auditgroup_a11y-navigation_pass': auditGroupCountOfMedianLhr('a11y-navigation', 'pass'),
+  'auditgroup_a11y-navigation_fail': auditGroupCountOfMedianLhr('a11y-navigation', 'fail'),
+  'auditgroup_a11y-navigation_na': auditGroupCountOfMedianLhr('a11y-navigation', 'na'),
+  'auditgroup_a11y-aria_pass': auditGroupCountOfMedianLhr('a11y-aria', 'pass'),
+  'auditgroup_a11y-aria_fail': auditGroupCountOfMedianLhr('a11y-aria', 'fail'),
+  'auditgroup_a11y-aria_na': auditGroupCountOfMedianLhr('a11y-aria', 'na'),
+  'auditgroup_a11y-language_pass': auditGroupCountOfMedianLhr('a11y-language', 'pass'),
+  'auditgroup_a11y-language_fail': auditGroupCountOfMedianLhr('a11y-language', 'fail'),
+  'auditgroup_a11y-language_na': auditGroupCountOfMedianLhr('a11y-language', 'na'),
+  'auditgroup_a11y-audio-video_pass': auditGroupCountOfMedianLhr('a11y-audio-video', 'pass'),
+  'auditgroup_a11y-audio-video_fail': auditGroupCountOfMedianLhr('a11y-audio-video', 'fail'),
+  'auditgroup_a11y-audio-video_na': auditGroupCountOfMedianLhr('a11y-audio-video', 'na'),
+  'auditgroup_a11y-tables-lists_pass': auditGroupCountOfMedianLhr('a11y-tables-lists', 'pass'),
+  'auditgroup_a11y-tables-lists_fail': auditGroupCountOfMedianLhr('a11y-tables-lists', 'fail'),
+  'auditgroup_a11y-tables-lists_na': auditGroupCountOfMedianLhr('a11y-tables-lists', 'na'),
+  'auditgroup_seo-mobile_pass': auditGroupCountOfMedianLhr('seo-mobile', 'pass'),
+  'auditgroup_seo-mobile_fail': auditGroupCountOfMedianLhr('seo-mobile', 'fail'),
+  'auditgroup_seo-mobile_na': auditGroupCountOfMedianLhr('seo-mobile', 'na'),
+  'auditgroup_seo-content_pass': auditGroupCountOfMedianLhr('seo-content', 'pass'),
+  'auditgroup_seo-content_fail': auditGroupCountOfMedianLhr('seo-content', 'fail'),
+  'auditgroup_seo-content_na': auditGroupCountOfMedianLhr('seo-content', 'na'),
+  'auditgroup_seo-crawl_pass': auditGroupCountOfMedianLhr('seo-crawl', 'pass'),
+  'auditgroup_seo-crawl_fail': auditGroupCountOfMedianLhr('seo-crawl', 'fail'),
+  'auditgroup_seo-crawl_na': auditGroupCountOfMedianLhr('seo-crawl', 'na'),
 };
 
 // Keep the export separate from declaration to enable tsc to typecheck the `@type` annotation.

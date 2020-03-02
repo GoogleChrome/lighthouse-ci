710b73f466e62680be5022285797e2a32cc03dbd
Patrick Hulce <patrick.hulce@gmail.com>
2020-03-02 16:26:47 -0600

    tests(server): update storyshots for axis improvements

--- packages/server/src/ui/routes/project-dashboard-redesign/graphs/category-score/category-score-graph.stories.jsx
+++ packages/server/src/ui/routes/project-dashboard-redesign/graphs/category-score/category-score-graph.stories.jsx
@@ -33,6 +33,8 @@ const createBuild = id => ({
   hash: '',
   branch: 'master',
   runAt: runAt(id),
+  createdAt: runAt(id),
+  updatedAt: runAt(id),
 });
 
 /** @param {{id: number, min?: number, max?: number, avg: number}} props @return {Array<import('./category-score-graph').StatisticWithBuild>} */

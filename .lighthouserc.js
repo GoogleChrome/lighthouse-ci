323ba53c20c2cdd357373c235c75c4c69e2d3e17
Patrick Hulce <patrick.hulce@gmail.com>
2020-05-20 12:17:26 -0500

    chore(ci): upgrade to lighthouserc.js

---
+++ .lighthouserc.js
@@ -0,0 +1,45 @@
+/**
+ * @license Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+'use strict';
+
+module.exports = {
+  ci: {
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'dom-size': ['error', {maxNumericValue: 3000}],
+
+        'uses-rel-preload': 'off',
+        'uses-responsive-images': 'off',
+        'uses-rel-preconnect': 'off',
+        'offscreen-images': 'off',
+        'unused-javascript': 'off',
+
+        label: 'off',
+        'content-width': 'off',
+        'color-contrast': 'off',
+        bypass: 'off',
+        'tap-targets': 'off',
+
+        'apple-touch-icon': 'off',
+        'maskable-icon': 'off',
+        'installable-manifest': 'off',
+        'offline-start-url': 'off',
+        'service-worker': 'off',
+        'splash-screen': 'off',
+        'themed-omnibox': 'off',
+        'works-offline': 'off',
+      },
+    },
+    upload: {
+      urlReplacementPatterns: [
+        's/[0-9a-f]{12}$/HASH/',
+        's#:[0-9]{3,5}/#:PORT/#',
+        's/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/UUID/ig',
+      ],
+    },
+  },
+};

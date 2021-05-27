55df7731561e69740c836254809c814c6509972b
Tom Hare <colourgarden@users.noreply.github.com>
2021-05-27 11:00:25 +0200

    Update packages/cli/src/open/open.js
    Co-authored-by: Patrick Hulce <patrick.hulce@gmail.com>

--- packages/cli/src/open/open.js
+++ packages/cli/src/open/open.js
@@ -39,7 +39,7 @@ async function runCommand(options) {
   }
 
   for (const lhr of representativeLhrs) {
-    if (options.url && (lhr.finalUrl !== options.url && !options.url.some(item => lhr.finalUrl.includes(item)))) continue;
+    if (targetUrls.size && !targetUrls.has(lhr.finalUrl)) continue;
 
     process.stdout.write(`Opening median report for ${lhr.finalUrl}...\n`);
     const tmpFile = tmp.fileSync({postfix: '.html'});

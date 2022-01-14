633b2bc74316f19d0452dc7f06ac0ff83c6d9c97
Connor Clark <cjamcl@gmail.com>
2022-01-14 15:13:08 -0800

    update

--- packages/cli/src/collect/puppeteer-manager.js
+++ packages/cli/src/collect/puppeteer-manager.js
@@ -85,7 +85,7 @@ class PuppeteerManager {
     // Otherwise, check to see if the expected puppeteer download exists.
     const puppeteer = PuppeteerManager._requirePuppeteer();
     const puppeteerUnknown = /** @type {unknown} */ (puppeteer);
-    const pupppeteerNode = /** @type {PuppeteerNode|undefined} */ (puppeteerUnknown);
+    const pupppeteerNode = /** @type {import('puppeteer').PuppeteerNode | undefined} */ (puppeteerUnknown);
     const chromiumPath = pupppeteerNode && pupppeteerNode.executablePath();
     return chromiumPath && fs.existsSync(chromiumPath) ? chromiumPath : undefined;
   }

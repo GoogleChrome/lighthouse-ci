97a72b65e84ace20187a0268829c9556705f8551
Connor Clark <cjamcl@gmail.com>
2022-01-20 13:33:34 -0800

    hmm

--- packages/server/test/e2e/project-dashboard-mixed-v5-v6.test.js
+++ packages/server/test/e2e/project-dashboard-mixed-v5-v6.test.js
@@ -9,6 +9,73 @@
 
 const {shouldRunE2E, emptyTest} = require('../test-utils.js');
 
+// This injects a box into the page that moves with the mouse;
+// Useful for debugging
+async function installMouseHelper(page) {
+  await page.evaluate(() => {
+    // Install mouse helper only for top-level frame.
+    if (window !== window.parent) return;
+    const box = document.createElement('puppeteer-mouse-pointer');
+    const styleElement = document.createElement('style');
+    styleElement.innerHTML = `
+      puppeteer-mouse-pointer {
+        pointer-events: none;
+        position: absolute;
+        top: 0;
+        z-index: 10000;
+        left: 0;
+        width: 20px;
+        height: 20px;
+        background: rgba(0,0,0,.4);
+        border: 1px solid white;
+        border-radius: 10px;
+        margin: -10px 0 0 -10px;
+        padding: 0;
+        transition: background .2s, border-radius .2s, border-color .2s;
+      }
+      puppeteer-mouse-pointer.button-1 {
+        transition: none;
+        background: rgba(0,0,0,0.9);
+      }
+      puppeteer-mouse-pointer.button-2 {
+        transition: none;
+        border-color: rgba(0,0,255,0.9);
+      }
+      puppeteer-mouse-pointer.button-3 {
+        transition: none;
+        border-radius: 4px;
+      }
+      puppeteer-mouse-pointer.button-4 {
+        transition: none;
+        border-color: rgba(255,0,0,0.9);
+      }
+      puppeteer-mouse-pointer.button-5 {
+        transition: none;
+        border-color: rgba(0,255,0,0.9);
+      }
+    `;
+    document.head.appendChild(styleElement);
+    document.body.appendChild(box);
+    document.addEventListener('mousemove', event => {
+      box.style.left = event.pageX + 'px';
+      box.style.top = event.pageY + 'px';
+      updateButtons(event.buttons);
+    }, true);
+    document.addEventListener('mousedown', event => {
+      updateButtons(event.buttons);
+      box.classList.add('button-' + event.which);
+    }, true);
+    document.addEventListener('mouseup', event => {
+      updateButtons(event.buttons);
+      box.classList.remove('button-' + event.which);
+    }, true);
+    function updateButtons(buttons) {
+      for (let i = 0; i < 5; i++)
+        box.classList.toggle('button-' + i, buttons & (1 << i));
+    }
+  });
+}
+
 describe('Project dashboard', () => {
   if (!shouldRunE2E()) return emptyTest();
 
@@ -42,19 +109,28 @@ describe('Project dashboard', () => {
     });
 
     it('should render graphs for previously unavailable data', async () => {
-      const {height, left} = await state.page.evaluate(() => {
+      await installMouseHelper(state.page);
+      const {x, y} = await state.page.evaluate(() => {
         const graphs = Array.from(document.querySelectorAll('.metric-line-graph__graph'));
         if (!graphs.length) throw new Error('Should have found 2 metric graphs');
-        const {top, bottom, left} = graphs[1].getBoundingClientRect();
-        window.scrollTo({top: top - 50});
-        return {height: bottom - top, left};
+
+        window.scrollTo({top: graphs[1].getBoundingClientRect().top - 50});
+        const rect = graphs[1].getBoundingClientRect();
+        return {x: rect.left + rect.width / 2, y: rect.top + rect.height / 2};
       });
 
-      await state.page.mouse.move(left * 1.5, 50 + height * 0.8);
-      await state.page.waitFor(500);
+      await state.page.mouse.move(x, y);
+      await state.page.focus('.metric-line-graph__graph'); // ?
     });
 
     it('should look correct on hover', async () => {
+      // await state.page.waitFor(1000 * 3);
+      // This failure makes no sense.
+      // Worked before with parcel, but not now with esbuild ?
+      // When running debug mode
+      //   DEBUG=1 yarn jest project-dashboard-mixed-v5-v6
+      // the card shows on hover. but the browser viewport moves about,
+      // perhaps causing the card to stop showing?
       expect(await state.page.screenshot({fullPage: false})).toMatchImageSnapshot();
     });
   });

ccf19ac73e153c9db20d66740a7e78b6e587c992
Eliasvdb <eliasvdb8@gmail.com>
2022-02-10 10:05:00 +0100

    Added puppeteer link and simple puppeteer test script

---
+++ scripts/puppeteer-test.js
@@ -0,0 +1,13 @@
+/**
+ * @param {puppeteer.Browser} browser
+ * @param {{url: string, options: LHCI.CollectCommand.Options}} context
+ */
+module.exports = async (browser, context) => {
+  // launch browser for LHCI
+  const page = await browser.newPage();
+
+  await page.goto("https://example.com");
+
+  // close session for next run
+  await page.close();
+};

c7304845b7a114ff250a5556004cc7bf7144c9df
Vikas Agarwal <vikas@propertyguru.tech>
2022-04-19 11:05:15 +0530

    fix: No need to change thresholds as scores are already accounting them.

--- packages/server/src/ui/components/score-icon.jsx
+++ packages/server/src/ui/components/score-icon.jsx
@@ -6,40 +6,11 @@
 
 import {h} from 'preact';
 
-
-/** @type {Record<LHCI.AssertCommand.Budget.TimingMetric, [number, number]>} */
-const SCORE_LEVEL_METRIC_THRESHOLDS = {
-  'first-contentful-paint': [2000, 4000],
-  'first-meaningful-paint': [2000, 4000],
-  'largest-contentful-paint': [2000, 4000],
-  'first-cpu-idle': [3000, 7500],
-  interactive: [3000, 7500],
-  'speed-index': [3000, 6000],
-  'total-blocking-time': [300, 600],
-  'cumulative-layout-shift': [0.1, 0.25],
-  'max-potential-fid': [100, 250],
-};
-
-/** @param {{score: number, diffs?: LHCI.AuditDiff[]}} props */
+/** @param {{score: number}} props */
 export const ScoreIcon = props => {
-  const { score, diffs } = props;
-  let result = score;
-  const numericDiff = diffs && diffs.find(diff => diff.type === 'numericValue');
-  if (numericDiff && numericDiff.type === 'numericValue') {
-    // @ts-ignore
-    const thresholds = SCORE_LEVEL_METRIC_THRESHOLDS[numericDiff.auditId];
-    if (thresholds) {
-      if (numericDiff.compareValue <= thresholds[0]) {
-        result = 0.9;
-      } else if (numericDiff.compareValue <= thresholds[1]) {
-        result = 0.5;
-      } else {
-        result = 0.49;
-      }
-    }
-  }
-  if (result >= 0.9) return <i className="lh-score-pass" />;
-  if (result >= 0.5) return <i className="lh-score-average" />;
+  const score = props.score;
+  if (score >= 0.9) return <i className="lh-score-pass" />;
+  if (score >= 0.5) return <i className="lh-score-average" />;
   return (
     <i className="lh-score-fail">
       <svg viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">

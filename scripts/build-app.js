555022adb4a9b9bc586598719c00e2a8ab9b95aa
Paul Irish <commits@paul.irish>
2022-04-29 16:07:58 -0700

    um i think it works everywhere

--- scripts/build-app.js
+++ scripts/build-app.js
@@ -12,7 +12,8 @@ const esbuild = require('esbuild');
 const command = process.argv[2];
 const entryPoint = process.argv[3];
 const outdir = process.argv[4];
-const publicPath = process.argv[5] || './';
+const defaultPublicPath = undefined;
+const publicPath = process.argv[5] || defaultPublicPath;
 
 if (!command || !entryPoint || !outdir) {
   throw new Error('missing args');
@@ -34,10 +35,10 @@ function insertCollectedStyles(result) {
   if (stylesheets.length > 1) throw new Error('expected at most one generated stylesheet');
 
   const href =
-    publicPath === './'
+    publicPath === defaultPublicPath
       ? path.relative(outdir, stylesheet)
       : `/app/${path.relative(outdir, stylesheet)}`;
-  console.log({href, outdir, stylesheet});
+
   const htmls = Object.keys(result.metafile.outputs).filter(o => o.endsWith('.html'));
   const html = htmls[0];
   if (htmls.length !== 1) throw new Error('expected exactly one generated html');
@@ -53,12 +54,34 @@ function insertCollectedStyles(result) {
 /**
  * @param {esbuild.BuildResult} result
  */
-function fixScriptSrc(result) {
-  if (publicPath === './') return;
+function adjustStylesheets(result) {
   if (!result.metafile) throw new Error('expected metafile');
 
+  const fonts = Object.keys(result.metafile.outputs).filter(o => o.endsWith('.woff'));
+  const font = fonts[0];
+  if (fonts.length === 0) return;
+
+  // todo loop over all???
+
+  const csss = Object.keys(result.metafile.outputs).filter(o => o.endsWith('.css'));
+  const css = csss[0];
+  if (csss.length !== 1) throw new Error('expected exactly one generated css');
+
+  const cssText = fs.readFileSync(css, 'utf-8');
+  const newCssText = cssText.replace(/\/iife\//g, 'XXXXX');
+  fs.writeFileSync(css, newCssText);
+}
+
+/**
+ * @param {esbuild.BuildResult} result
+ */
+function fixScriptSrc(result) {
   const htmls = Object.keys(result.metafile.outputs).filter(o => o.endsWith('.html'));
   const html = htmls[0];
+  console.log({html});
+  if (publicPath === defaultPublicPath) return;
+  if (!result.metafile) throw new Error('expected metafile');
+
   if (htmls.length !== 1) throw new Error('expected exactly one generated html');
 
   const needle = '<script src="';
@@ -69,13 +92,24 @@ function fixScriptSrc(result) {
   fs.writeFileSync(html, newHtmlText);
 }
 
+/**
+ * @param {esbuild.BuildResult} result
+ */
+function duplicateSVGs(result) {
+
+
+}
+
 async function main() {
   const htmlPlugin = (await import('@chialab/esbuild-plugin-html')).default;
 
+  console.log({entryPoint})
   /** @type {esbuild.BuildOptions} */
   const buildOptions = {
     entryPoints: [entryPoint],
-    entryNames: '[dir]/[name]-[hash]',
+    entryNames: '[name]',
+    assetNames: 'assets/[name]-[hash]',
+
     // @ts-expect-error: Stuck on older version of this plugin, because newest has issues
     // resolving script relative to the html. and creates an index-[hash].html file instead
     // of the expected index.html ... really all kinds of broken.
@@ -91,6 +125,7 @@ async function main() {
     },
     publicPath,
     bundle: true,
+    format: 'esm',
     outdir,
     minify: false,
     sourcemap: true,
@@ -108,9 +143,13 @@ async function main() {
         : undefined,
   };
 
+  console.log(buildOptions); 
   const result = await esbuild.build(buildOptions);
-  insertCollectedStyles(result);
-  fixScriptSrc(result);
+  // insertCollectedStyles(result);
+  // fixScriptSrc(result);
+  // duplicateSVGs(result);
+  // adjustStylesheets(result);
+  console.log('Built.', new Date());
 }
 
 main().catch(err => {

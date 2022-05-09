72270e7cfed4018c414b4dc5251d87fc35b5eae3
Paul Irish <paulirish@users.noreply.github.com>
2022-05-09 14:00:17 -0700

    fix standalone viewer build via build-app adjustments (#786)

--- scripts/build-app.js
+++ scripts/build-app.js
@@ -6,13 +6,13 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
+
 const esbuild = require('esbuild');
 
 const command = process.argv[2];
 const entryPoint = process.argv[3];
 const outdir = process.argv[4];
-const publicPath = process.argv[5] || '/';
+const publicPath = process.argv[5];
 
 if (!command || !entryPoint || !outdir) {
   throw new Error('missing args');
@@ -25,61 +25,31 @@ if (!['build', 'watch'].includes(command)) {
 /**
  * @param {esbuild.BuildResult} result
  */
-function insertCollectedStyles(result) {
-  if (!result.metafile) throw new Error('expected metafile');
-
-  const stylesheets = Object.keys(result.metafile.outputs).filter(o => o.endsWith('.css'));
-  const stylesheet = stylesheets[0];
-  if (stylesheets.length === 0) return;
-  if (stylesheets.length > 1) throw new Error('expected at most one generated stylesheet');
-
-  const href = publicPath === '/' ?
-    path.relative(outdir, stylesheet) :
-    `/app/${path.relative(outdir, stylesheet)}`;
-
-  const htmls = Object.keys(result.metafile.outputs).filter(o => o.endsWith('.html'));
-  const html = htmls[0];
-  if (htmls.length !== 1) throw new Error('expected exactly one generated html');
-
-  const needle = '<!-- %CSS% -->';
-  const htmlText = fs.readFileSync(html, 'utf-8');
-  if (!htmlText.includes(needle)) throw new Error(`expected ${needle} in html`);
-
-  const newHtmlText = htmlText.replace(needle, `<link rel="stylesheet" href="${href}" />`);
-  fs.writeFileSync(html, newHtmlText);
-}
-
-/**
- * @param {esbuild.BuildResult} result
- */
- function fixScriptSrc(result) {
-  if (publicPath === '/') return;
+function fixHtmlSubresourceUrls(result) {
+  if (publicPath !== '/app') return;
   if (!result.metafile) throw new Error('expected metafile');
 
   const htmls = Object.keys(result.metafile.outputs).filter(o => o.endsWith('.html'));
   const html = htmls[0];
   if (htmls.length !== 1) throw new Error('expected exactly one generated html');
 
-  const needle = '<script src="';
   const htmlText = fs.readFileSync(html, 'utf-8');
-  if (!htmlText.includes(needle)) throw new Error(`expected ${needle} in html`);
-
-  const newHtmlText =
-    htmlText.replace(needle, `${needle}/app/`);
+  const newHtmlText = htmlText
+    .replace('<script src="chunks/', '<script src="/app/chunks/')
+    .replace('<link rel="stylesheet" href="chunks/', '<link rel="stylesheet" href="/app/chunks/');
   fs.writeFileSync(html, newHtmlText);
 }
 
 async function main() {
   const htmlPlugin = (await import('@chialab/esbuild-plugin-html')).default;
-  
+
   /** @type {esbuild.BuildOptions} */
   const buildOptions = {
     entryPoints: [entryPoint],
-    entryNames: '[dir]/[name]-[hash]',
-    // @ts-expect-error: Stuck on older version of this plugin, because newest has issues
-    // resolving script relative to the html. and creates an index-[hash].html file instead
-    // of the expected index.html ... really all kinds of broken.
-    // Anyway, this is a type error because of slightly different esbuild version type for Plugin.
+    entryNames: '[name]',
+    assetNames: 'assets/[name]-[hash]',
+    // Defined chunknames breaks the viewer (probably cuz the -plugin-html), but pairs with fixHtmlSubresourceUrls.
+    chunkNames: publicPath ? `chunks/[name]-[hash]` : undefined,
     plugins: [htmlPlugin()],
     loader: {
       '.svg': 'file',
@@ -95,22 +65,27 @@ async function main() {
     minify: true,
     sourcemap: true,
     jsxFactory: 'h',
-    watch: command === 'watch' ? {
-      onRebuild(err, result) {
-        if (!err && result) {
-          insertCollectedStyles(result);
-          fixScriptSrc(result);
-        }
-      },
-    } : undefined,
+    watch:
+      command === 'watch'
+        ? {
+            onRebuild(err, result) {
+              if (!err && result) {
+                fixHtmlSubresourceUrls(result);
+              }
+            },
+          }
+        : undefined,
   };
 
   const result = await esbuild.build(buildOptions);
-  insertCollectedStyles(result);
-  fixScriptSrc(result);
+  fixHtmlSubresourceUrls(result);
+
+  console.log('Built.', new Date());
+  if (result.errors.length) console.error(result.errors);
+  if (result.warnings.length) console.warn(result.warnings);
 }
 
-main().catch((err) => {
+main().catch(err => {
   console.error(err);
   process.exit(1);
 });

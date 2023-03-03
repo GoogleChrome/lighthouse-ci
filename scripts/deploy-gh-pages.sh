179512026df6bd96417b1f34d162e412bc39487b
Paul Irish <commits@paul.irish>
2023-03-02 17:13:22 -0800

    copy fixtures

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -29,6 +29,7 @@ git checkout -b gh-pages
 yarn clean
 yarn build
 cp -R packages/viewer/dist ./viewer
+cp -R packages/server/test/fixtures ./viewer/fixtures
 
 # Create and push the deploy commit
 git add ./viewer
